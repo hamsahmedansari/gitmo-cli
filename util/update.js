8e7bbd9711a736e7a22c610a183dd56a46f81dff
hamsahmedansari <hams.ahmed.ansari95@gmail.com>
2019-04-25 22:25:38 +0500

    update: getting modified files

--- util/update.js
+++ util/update.js
@@ -14,11 +14,29 @@ const getModifiedFiles = () => {
   ).trim();
   const tempArr = [
     `(use "git checkout -- <file>..." to discard changes in working directory)`,
-    `Untracked files:`
+    `Untracked files:`,
+    `no changes added to commit (use "git add" and/or "git commit -a")`
   ];
-  const indexOf = [gitCode.indexOf(tempArr[0]), gitCode.indexOf(tempArr[1])];
-  if (gitCode.includes("modified")) return false;
-  return String(gitCode.slice(indexOf[0] + tempArr[0].length, indexOf[1]))
+
+  let firstIndex = gitCode.indexOf(tempArr[0]) + tempArr[0].length;
+
+  if (!gitCode.includes(tempArr[0])) {
+    console.log("return false !!!");
+    return false;
+  }
+  let secondIndex;
+  if (gitCode.includes(tempArr[1])) {
+    console.log("i am true");
+
+    secondIndex = gitCode.indexOf(tempArr[1]);
+  } else if (gitCode.includes(tempArr[2])) {
+    console.log("i am true from 2");
+    secondIndex = gitCode.indexOf(tempArr[2]);
+  } else {
+    console.log("return false");
+    return false;
+  }
+  return String(gitCode.slice(firstIndex, secondIndex))
     .replace(/modified:/g, "")
     .replace(/\t/g, "")
     .replace(/ /g, "")
